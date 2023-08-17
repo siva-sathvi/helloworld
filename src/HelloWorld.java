b783577684b96b9408a8dec250b7b329a5da3697
siva-sathvi <parvathib471@gmail.com>
2023-08-17 22:52:31 +0530

    updated the fine

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,7 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("hello world");		// TODO Auto-generated method stub
+		System.out.println("hello world i am lakshmi");		// TODO Auto-generated method stub
 
 	}
 
