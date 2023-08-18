d5cb3aa4d3de67ca5660777eeb71e0c68d8ae84b
siva-sathvi <parvathib471@gmail.com>
2023-08-18 14:26:17 +0530

    my task compleated

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("hello world i am lakshmi");		// TODO Auto-generated method stub
-
+System.out.println("i am chitti ,working this project");
 	}
 
 }
